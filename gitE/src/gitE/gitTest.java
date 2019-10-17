01a5f92848d91ca1128fcb5c242c3b03fa2ec3e9
Yoo <yoo@218.38.137.124>
2019-10-17 15:48:30 +0900

    FIX

--- gitE/src/gitE/gitTest.java
+++ gitE/src/gitE/gitTest.java
@@ -5,6 +5,8 @@ public class gitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		//P
+		System.out.println("ECLIPSE . GIT");
 
 }
 }
