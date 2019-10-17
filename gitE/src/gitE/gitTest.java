577d5060b8e0c76f7e4f54f0d9d1c817c3044404
YooSH-DY <55770677+YooSH-DY@users.noreply.github.com>
2019-10-17 16:26:39 +0900

    GH

--- gitE/src/gitE/gitTest.java
+++ gitE/src/gitE/gitTest.java
@@ -5,14 +5,10 @@ public class gitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-//<<<<<<< HEAD
 		//P
 		System.out.println("ECLIPSE . GIT");
-//=======
 		//Commit.
 		System.out.println("NewBranch");
-		//COMP.1
-//>>>>>>> refs/remotes/macY/master
-
+		//COMP.
 }
 }
